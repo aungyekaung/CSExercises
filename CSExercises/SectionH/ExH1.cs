ca3a5e1c9d9343bb1a302f6640b0f630137204db
Aung Ye Kaung <aungyekaung.myanmar@gmail.com>
2016-10-02 21:15:30 +0800

    H2

--- CSExercises/SectionH/ExH1.cs
+++ CSExercises/SectionH/ExH1.cs
@@ -1,15 +1,61 @@
-﻿namespace CSExercises
+﻿using System;
+
+namespace CSExercises
 {
     public class ExH1
     {
+        static void Main (string[] args)
+        {
+            for (int i = 0; i< 5; i++)
+            {
+                Console.WriteLine("Emter S1:");
+                string s1 = Console.ReadLine();
+
+                Console.WriteLine("Enter S2:");
+                string s2 = Console.ReadLine();
+
+                Console.WriteLine("Output is" + InString(s1, s2));
+            }
+        }
         public static bool InString(string s1, string s2)
         {
-            //YOUR CODE HERE
-            return false;
+            s1 = ModifyString(s1);
+            s2 = ModifyString(s2);
+            int count = 0;
+            Console.WriteLine(s1 + s2);
+            bool result = false;
 
+            for (int i = 0; i < s1.Length; i++ )
+            {
+                if (s2.Substring(0,1) == s1.Substring(i,1))
+                {
+                    if (s2 == s1.Substring(i,s2.Length))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    result = false;
+                }
+            }
 
+            return result;
+            
+        }
 
+        public static string ModifyString(string phrase)
+        {
+            string[] punc = { " ", ".", ",", "'", ";", ":", "!", "@", "\"" };
+            phrase = phrase.ToLower();
 
+            for (int k = 0; k < punc.Length; k++)
+            {
+                phrase = phrase.Replace(punc[k], string.Empty);
+            }
+
+            return phrase;
         }
     }
 }
--- CSExercises/SectionH/ExH2.cs
+++ CSExercises/SectionH/ExH2.cs
@@ -1,4 +1,5 @@
-﻿namespace CSExercises
+﻿using System;
+namespace CSExercises
 {
     public class ExH2
     {
@@ -7,14 +8,57 @@
         //indicate the starting position of the word s2 in s1.  If the word does not occur 
         //the return value should be –1.
 
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Emter S1:");
+                string s1 = Console.ReadLine();
+
+                Console.WriteLine("Enter S2:");
+                string s2 = Console.ReadLine();
+
+                Console.WriteLine("Output is" + FindWord(s1, s2));
+            }
+        }
         public static int FindWord(string s1, string s2)
         {
-            //YOUR CODE HERE
-            return 0;
+            s1 = ModifyString(s1);
+            s2 = ModifyString(s2);
+            int result = -1;
+            Console.WriteLine(s1 + s2);
 
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s2.Substring(0, 1) == s1.Substring(i, 1))
+                {
+                    if (s2 == s1.Substring(i, s2.Length))
+                    {
+                        result = i;
+                        break;
+                    }
+                }
+                else
+                {
+                    result = -1;
+                }
+            }
 
+            return result;
 
+        }
 
+        public static string ModifyString(string phrase)
+        {
+            string[] punc = { " ", ".", ",", "'", ";", ":", "!", "@", "\"" };
+            phrase = phrase.ToLower();
+
+            for (int k = 0; k < punc.Length; k++)
+            {
+                phrase = phrase.Replace(punc[k], string.Empty);
+            }
+
+            return phrase;
         }
     }
 }
